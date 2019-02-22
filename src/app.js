95b410cfbfc98b183f65a7d44c7584f7946adb2f
Zoljargal Fallows <zol.fallows@hotmail.com>
2019-02-22 23:10:47 +0000

    bookmarks server complete

--- src/app.js
+++ src/app.js
@@ -29,15 +29,14 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
-app.get('/', (req, res) => {
-  res.send('Hello, world!');});
+
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === 'production') {
     response = { error: { message: 'server error' } };
   } else {
-    console.error(error);
+    // console.error(error);
     response = { message: error.message, error };
   }
   res.status(500).json(response);
--- src/bookmark/bookmark-router.js
+++ src/bookmark/bookmark-router.js
@@ -1,38 +1,46 @@
 'use strict';
-const express = require('express');
-const uuid = require('uuid/v4');
-const logger = require('../logger');
-const { bookmarks } = require('../store');
+const express    = require('express');
+const uuid       = require('uuid/v4');
+const logger     = require('../logger');
+const  bookmarks = require('../store');
 
 const bookmarkRouter = express.Router();
 const bodyParser = express.json();
 
-bookmarkRouter.route('/bookmarks').get((req, res) => {
-  res.json(bookmarks);
-}).post(bodyParser, (req, res) => {
-  const { title, content } = req.body;
+bookmarkRouter
+  .route('/bookmarks')
+  .get((req, res) => {
+    res.json(bookmarks);
+  })
+  .post(bodyParser, (req, res) => {
+    const { title, content } = req.body;
 
-  if (!title) {
-    logger.error('Title is required');
-    return res
-      .status(400)
-      .send('Invalid data');
-  }
+    if (!title) {
+      logger.error('Title is required');
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
     
-  if (!content) {
-    logger.error('Content is required');
-    return res
-      .status(400)
-      .send('Invalid data');
-  }
-  const id = uuid();
-  const bookmark = {id, title, content};
-  bookmarks.push(bookmark);
-  
-  logger.info(`bookmark with id ${id} created`);
-  res.status(201).location(`/http://localhost:8000/bookmarks/${id}`).json(bookmark);
+    if (!content) {
+      logger.error('Content is required');
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
+    const id = uuid();
+    const bookmark = {id, title, content};
 
-});
+    bookmarks.push(bookmark);
+  
+    logger.info(`bookmark with id ${id} created`);
+
+    res
+      .status(201)
+      .location(`/http://localhost:8000/bookmarks/${id}`)
+      .json(bookmark);
+
+  });
 
 bookmarkRouter
   .route('/bookmarks/:id')
@@ -40,7 +48,6 @@ bookmarkRouter
     const { id } = req.params;
     const bookmark = bookmarks.find(b => b.id === id);
 
-    // make sure we found a bookmark
     if (!bookmark) {
       logger.error(`Bookmark with id ${id} not found.`);
       return res
@@ -49,12 +56,17 @@ bookmarkRouter
     }
     
     res.json(bookmark);
-  }).delete((req, res) => {
+  })
+  .delete((req, res) => {
     const { id } = req.params;
 
     const bookmarkIndex = bookmarks.findIndex(b => b.id === id);
+
     bookmarks.splice(bookmarkIndex, 1);
-    res.status(204).end();
+
+    res
+      .status(204)
+      .end();
   });
 
 module.exports = bookmarkRouter;
